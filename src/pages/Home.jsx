2fd04e8872c78f0f91e4d232d274183fd20ead4a
Phil Tietjen <tietjen.philip@gmail.com>
2020-09-05 19:51:39 -0400

    feat(Home): add grid of movie posters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 
+import usePopularMovies from "hooks/usePopularMovies";
+
+import MoviePoster from "components/MoviePoster";
+
 export function Home() {
+  const { data } = usePopularMovies();
+
   return (
     <>
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold leading-tight text-gray-900">
-            Home
+            Popular Movies
           </h1>
         </div>
       </header>
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-12">
+              {data &&
+                data.results.map((movie) => {
+                  return (
+                    <MoviePoster
+                      key={movie.id}
+                      title={movie.title}
+                      imageAlt={movie.title}
+                      imagePath={movie.poster_path}
+                      imageWidth="w500"
+                    />
+                  );
+                })}
+            </div>
           </div>
         </div>
       </main>
