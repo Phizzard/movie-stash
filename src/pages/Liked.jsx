f69d28bc18cb98b2ad7ef9e71d5c15c539ba88dc
Phil Tietjen <tietjen.philip@gmail.com>
2020-09-05 22:49:37 -0400

    feat(Liked):  display liked movies

--- src/pages/Liked.jsx
+++ src/pages/Liked.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 
+import useLikedMovies from "hooks/useLikedMovies";
+
+import MoviePoster from "components/MoviePoster";
+
 export function Liked() {
+  const [movies, setLikedMovies] = useLikedMovies();
+
+  function handleOnHeartClick(movieData) {
+    let newLikedMovies = {};
+
+    if (!movieData.isLiked) {
+      newLikedMovies = {
+        ...movies,
+        [movieData.id]: { ...movieData, isLiked: true },
+      };
+    } else {
+      const { [movieData.id]: otherMovie, ...rest } = movies;
+      newLikedMovies = rest;
+    }
+    setLikedMovies(newLikedMovies);
+  }
+
   return (
     <>
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold leading-tight text-gray-900">
-            Liked
+            Movies You Liked
           </h1>
         </div>
       </header>
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-12">
+              {Object.values(movies).map((movie) => {
+                return (
+                  <MoviePoster
+                    key={movie.id}
+                    id={movie.id}
+                    title={movie.title}
+                    imageAlt={movie.title}
+                    imagePath={movie.imagePath}
+                    imageWidth="w500"
+                    onHeartClick={handleOnHeartClick}
+                    isLiked={movies[movie.id]}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
       </main>
