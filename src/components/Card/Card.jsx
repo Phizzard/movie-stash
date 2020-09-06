22b920ba5434d93f210aba69d5e3478e5725bec5
Phil Tietjen <tietjen.philip@gmail.com>
2020-09-05 22:46:42 -0400

    feat(Card): add relative position

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ children, ...props }) {
   return (
-    <div className="shadow rounded-lg" {...props}>
+    <div className="relative shadow rounded-lg" {...props}>
       {children}
     </div>
   );
