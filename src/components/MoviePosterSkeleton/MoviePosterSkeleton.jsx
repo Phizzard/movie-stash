2794fa24fa6e5b8e45b6adc058c32faaa4886a17
Phil Tietjen <tietjen.philip@gmail.com>
2020-09-06 21:28:21 -0400

    feat(MoviePosterSkeleton): create component

---
+++ src/components/MoviePosterSkeleton/MoviePosterSkeleton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MoviePosterSkeleton() {
+  return (
+    <div className="bg-gradient-to-tr from-blue-200 to-indigo-300 rounded shadow w-full h-64"></div>
+  );
+}
+
+export default MoviePosterSkeleton;
